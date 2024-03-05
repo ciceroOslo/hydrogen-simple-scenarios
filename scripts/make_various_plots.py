9bcaf53e45122dda1f1ac09749338448fdf20460
maritsandstad <marit.sandstad@astro.uio.no>
2024-03-05 14:23:04 +0100

    Adding more plotting

--- scripts/make_various_plots.py
+++ scripts/make_various_plots.py
@@ -19,65 +19,97 @@ from hydrogen_simple_scenarios import get_emissions_functions, scenario_info, ss
 
 data_path = "/mnt/c/Users/masan/Downloads/Input_for_scenarios/SSP_CMIP6_201811.csv"
 
-fig, axs = plt.subplots(nrows=1,ncols=2,squeeze=True,figsize=(18,10),sharey=False)
+def plot_co_emissions_and_scaled_hydrogen():
 
-years = ssp_data_extraction.get_years(data_path) 
-#Plot CO emissions:
-for scen,colour in scenario_info.scens_colours.items():
-    data_co = ssp_data_extraction.get_ts_component_sector_region_ssp(data_path, scen, "CO")
-    axs[0].plot(years, data_co, color = colour, label = scenario_info.scens_reverse[scen])
-    data_h2 = ssp_data_extraction.get_ts_component_sector_region_ssp(data_path, scen, "H2")
-    axs[1].plot(years, data_h2, color = colour, label = scenario_info.scens_reverse[scen])
+    fig, axs = plt.subplots(nrows=1,ncols=2,squeeze=True,figsize=(18,10),sharey=False)
 
-axs[0].set_ylabel('CO emissions [Tg CO/yr]')
-for i, ax in enumerate(axs):
-    ax.set_xlabel('Years')
-    ax.set_ylim(bottom=0)
-    axs[i].set_title(f"{chr(i+97)})", fontsize=20, loc='left')
-axs[1].set_ylabel('H2 emissions [Tg H2/yr]')
-axs[1].legend()
+    years = ssp_data_extraction.get_years(data_path) 
+    #Plot CO emissions:
+    for scen,colour in scenario_info.scens_colours.items():
+        data_co = ssp_data_extraction.get_ts_component_sector_region_ssp(data_path, scen, "CO")
+        axs[0].plot(years, data_co, color = colour, label = scenario_info.scens_reverse[scen])
+        data_h2 = ssp_data_extraction.get_ts_component_sector_region_ssp(data_path, scen, "H2")
+        axs[1].plot(years, data_h2, color = colour, label = scenario_info.scens_reverse[scen])
 
-axs[0].set_title('CO emissions from SSP database')
-axs[1].set_title('H2 emissions (scaled by CO)')
-plt.suptitle('Anthropogenic and biomass burning emissions',fontsize=25)
-plt.savefig("co_and_hydrogen_projections.png")
-plt.clf()
+    axs[0].set_ylabel('CO emissions [Tg CO/yr]')
+    for i, ax in enumerate(axs):
+        ax.set_xlabel('Years')
+        ax.set_ylim(bottom=0)
+        axs[i].set_title(f"{chr(i+97)})", fontsize=20, loc='left')
+    axs[1].set_ylabel('H2 emissions [Tg H2/yr]')
+    axs[1].legend()
 
-data_path = "/mnt/c/Users/masan/Downloads/Input_for_scenarios/SSP_IAM_V2_201811.csv"
+    axs[0].set_title('CO emissions from SSP database')
+    axs[1].set_title('H2 emissions (scaled by CO)')
+    plt.suptitle('Anthropogenic and biomass burning emissions',fontsize=25)
+    plt.savefig("co_and_hydrogen_projections.png")
+    plt.clf()
 
-fig, axs = plt.subplots(nrows=1,ncols=3,figsize=(22,8),sharey=False)
-years = ssp_data_extraction.get_years(data_path)
-print(years)
-for values in ssp_data_extraction.get_unique_scenarios_and_models(data_path):
-    data_energy, data_mass = ssp_data_extraction.get_ts_hydrogen_energy_and_mass(data_path, values[1], model=values[0])
-    axs[0].plot(years, data_energy, color='lightgray')
-    axs[1].plot(years, data_mass, color='lightgray')
+datapath_simpleh2 = os.path.join(os.path.dirname("__file__"), "..", "..", "simpleH2", "input")
+def make_nox_co_ratio_and_ch4_plot(scenarios):
+    fig, axs = plt.subplots(nrows=1,ncols=2,squeeze=True,figsize=(18,10),sharey=False)
+    for scen, colour in scenario_info.scens_colours.items():
+        if scen not in scenarios:
+            continue
+        data_co = pd.read_csv(os.path.join(datapath_simpleh2, f"co_emis_noburn_{scen}.csv"))
+        data_nox = pd.read_csv(os.path.join(datapath_simpleh2, f"nox_emis_noburn_{scen}.csv"))
+        data_ch4 = pd.read_csv(os.path.join(datapath_simpleh2, f"ch4_conc_{scen}.csv"))
+        axs[0].plot(data_ch4["Years"].to_numpy(), data_ch4["Emis"].to_numpy(), color=colour, label = scenario_info.scens_reverse[scen])
+        axs[1].plot(data_co["Years"].to_numpy(), data_nox["Emis"].to_numpy()/data_co["Emis"].to_numpy(), color=colour, label = scenario_info.scens_reverse[scen])
+    
+    for i,ax in enumerate(axs):
+        ax.set_xlabel('Years')
+        ax.set_xlim(left=1970, right=2050)
+        #ax.set_ylim(bottom=0)
+        ax.legend(fontsize=20)
+        #ax.xaxis.set_major_locator(loc)
+        axs[i].set_title(f"{chr(i+97)})", fontsize=20, loc='left')
+    axs[0].set_ylabel("Methane concentration [ppb]")
+    axs[1].set_ylabel("NOx/CO")
+    axs[0].set_title('Methane concentration', fontweight="bold")
+    axs[1].set_title('NOx/CO emissions', fontweight="bold")
+    plt.savefig("methane_and_nox_co_scenarios.png")
 
-for scen, colour in scenario_info.scens_colours.items():
-    data_energy, data_mass = ssp_data_extraction.get_ts_hydrogen_energy_and_mass(data_path, scen)
-    if not np.any(data_energy):
-        continue
-    axs[0].plot(years, data_energy, color = colour, label = scenario_info.scens_reverse[scen])
-    axs[1].plot(years, data_mass, color = colour, label = scenario_info.scens_reverse[scen])
-    axs[2].fill_between(years, data_mass*0.01, data_mass*0.1, color = colour, alpha = 0.2, label = scenario_info.scens_reverse[scen])
-    axs[2].plot(years, data_mass*0.01, "--", color=colour)
-    axs[2].plot(years, data_mass*0.1, "--", color=colour)
+data_path_iam = "/mnt/c/Users/masan/Downloads/Input_for_scenarios/SSP_IAM_V2_201811.csv"
+def make_hydrogen_energy_plot(): 
 
-axs[0].set_ylabel('Energy Hydrogen [EJ/yr]')
-axs[1].set_ylabel('Hydrogen [Tg/yr]')
-axs[2].set_ylabel('Hydrogen leakages [Tg/yr]')
-loc = mpl.ticker.MultipleLocator(base=2.0)
-for i,ax in enumerate(axs):
-    ax.set_xlabel('Years')
-    ax.set_ylim(bottom=0)
-    ax.legend(fontsize=20)
-    ax.xaxis.set_major_locator(loc)
-    axs[i].set_title(f"{chr(i+97)})", fontsize=20, loc='left')
+    fig, axs = plt.subplots(nrows=1,ncols=3,figsize=(22,8),sharey=False)
+    years = ssp_data_extraction.get_years(data_path)
+    print(years)
+    for values in ssp_data_extraction.get_unique_scenarios_and_models(data_path_iam):
+        data_energy, data_mass = ssp_data_extraction.get_ts_hydrogen_energy_and_mass(data_path_iam, values[1], model=values[0])
+        axs[0].plot(years, data_energy, color='lightgray')
+        axs[1].plot(years, data_mass, color='lightgray')
 
-axs[0].set_title('Energy')
-axs[1].set_title('Energy converted to mass')
-axs[2].set_title('1-10% leakage rate')
+    for scen, colour in scenario_info.scens_colours.items():
+        data_energy, data_mass = ssp_data_extraction.get_ts_hydrogen_energy_and_mass(data_path_iam, scen)
+        if not np.any(data_energy):
+            continue
+        axs[0].plot(years, data_energy, color = colour, label = scenario_info.scens_reverse[scen])
+        axs[1].plot(years, data_mass, color = colour, label = scenario_info.scens_reverse[scen])
+        axs[2].fill_between(years, data_mass*0.01, data_mass*0.1, color = colour, alpha = 0.2, label = scenario_info.scens_reverse[scen])
+        axs[2].plot(years, data_mass*0.01, "--", color=colour)
+        axs[2].plot(years, data_mass*0.1, "--", color=colour)
 
-plt.suptitle('Hydrogen Energy in SSP database',fontsize=25)
-plt.savefig("hydrogen_energy_projectsions")
-#plt.tight_layout()
+    axs[0].set_ylabel('Energy Hydrogen [EJ/yr]')
+    axs[1].set_ylabel('Hydrogen [Tg/yr]')
+    axs[2].set_ylabel('Hydrogen leakages [Tg/yr]')
+    loc = mpl.ticker.MultipleLocator(base=2.0)
+    for i,ax in enumerate(axs):
+        ax.set_xlabel('Years')
+        ax.set_ylim(bottom=0)
+        ax.legend(fontsize=20)
+        ax.xaxis.set_major_locator(loc)
+        axs[i].set_title(f"{chr(i+97)})", fontsize=20, loc='left')
+
+    axs[0].set_title('Energy')
+    axs[1].set_title('Energy converted to mass')
+    axs[2].set_title('1-10% leakage rate')
+
+    plt.suptitle('Hydrogen Energy in SSP database',fontsize=25)
+    plt.savefig("hydrogen_energy_projectsions")
+    #plt.tight_layout()
+
+##cenarios = ["ssp119", "ssp434", "ssp585"]
+make_hydrogen_energy_plot()
+#make_nox_co_ratio_and_ch4_plot(scenarios)
--- src/hydrogen_simple_scenarios/ssp_data_extraction.py
+++ src/hydrogen_simple_scenarios/ssp_data_extraction.py
@@ -5,11 +5,15 @@ import sys
 from .scenario_info import scens_reverse, scen_reverse_model
 from .get_emissions_functions import get_co_to_h2_factor_cmip6
 
-co_ssp_sectors = ["Agricultural Waste Burning", "Aircraft", "Energy Sector", "Forest Burning", "Grassland Burning", "Industrial Sector", "International Shipping", "Peat Burning", "Residential Commercial Other", "Transportation Sector", "Waste"]
+co_ssp_sectors_burning = ["Forest Burning", "Grassland Burning",  "Peat Burning"]
+co_ssp_sectors_AFOLU = co_ssp_sectors_burning +["Agricultural Waste Burning", "Agriculture"]
+co_ssp_sectors_industrial = ["Aircraft", "Energy Sector","Industrial Sector", "International Shipping", "Residential Commercial Other", "Transportation Sector", "Waste", "Solvents Production and Application"]
+co_ssp_sectors = co_ssp_sectors_AFOLU + co_ssp_sectors_industrial
 
-def get_data_for_component_sector_region_ssp(file, scen, comp, sector = "SUM", region="World", model="empty"):
+def get_data_for_component_sector_region_ssp(file, scen, comp, sector = "SUM", region="World", model="empty", filetype="SSP"):
     tot_data = pd.read_csv(file)
-    sector_string = get_sector_string(comp, sector)
+    tot_data.columns = map(str.upper, tot_data.columns)
+    sector_string = get_sector_string(comp, sector, filetype)
     if scen in scen_reverse_model:
         if "Energy" in sector:
             scen_q = scen_reverse_model[scen].split("_")[1]
@@ -18,32 +22,54 @@ def get_data_for_component_sector_region_ssp(file, scen, comp, sector = "SUM", r
         model = scen_reverse_model[scen].split("_")[0]
     else:
         scen_q = scen    
+    print(scen_q)
+    print(region)
+    print(model)
     cut_data = tot_data.query(f"SCENARIO == '{scen_q}' and REGION == '{region}' and MODEL == '{model}'")
+    print(cut_data.head())
+    print(sector_string)
     cut_data = cut_data.query(f"VARIABLE =='{sector_string}'")
+    print(cut_data.head())
     return cut_data
 
-def get_sector_string(comp, sector = "SUM"):
-    if sector == "SUM":
-        return f"CMIP6 Emissions|{comp}"
-    elif "Energy" in sector:
-        return f"{sector}|{comp}"
+def get_sector_string(comp, sector = "SUM", filetype="SSP"):
+    if filetype == "SSP":
+        opening = "CMIP6 Emissions|"
+    elif filetype == "RCMIP":
+        opening = "Emissions|"
     else:
-        return f"CMIP6 Emissions|{comp}|{sector}"
+        opening = ""
+    if sector == "SUM":
+        return f"{opening}{comp}"
+    if filetype == "RCMIP":
+        if sector in co_ssp_sectors_AFOLU:
+            return f"{opening}{comp}|MAGICC AFOLU|{sector}"
+        return f"{opening}{comp}|MAGICC Fossil and Industrial{sector}"
+    return f"{opening}{comp}|{sector}"
 
-def get_ts_component_sector_region_ssp(file, scen, comp, sector = "SUM", region = "World", model = "empty"):
+def get_ts_component_sector_region_ssp(file, scen, comp, sector = "SUM", region = "World", model = "empty", filetype="SSP"):
+    if filetype == "SSP":
+        meta_len = 5
+    elif filetype == "RCMIP":
+        meta_len = 7
+    else:
+        meta_len = 0
     if comp != "H2":
-        read_data = get_data_for_component_sector_region_ssp(file, scen, comp, sector = sector, region=region, model=model)
+        read_data = get_data_for_component_sector_region_ssp(file, scen, comp, sector = sector, region=region, model=model, filetype=filetype)
         if read_data.shape[0] == 0:
-            return np.zeros(read_data.shape[1]-5)
-        return read_data.iloc[0, 5:].to_numpy(dtype=float)
+            return np.zeros(read_data.shape[1]-meta_len)
+        return read_data.iloc[0, meta_len:].to_numpy(dtype=float)
     if sector != "SUM":
-        return get_data_for_component_sector_region_ssp(file, scen, "CO", sector = sector, region=region).iloc[0, 5:].to_numpy(dtype=float) * get_co_to_h2_factor_cmip6(sector)
-    timeseries = np.zeros(10)
+        return get_data_for_component_sector_region_ssp(file, scen, "CO", sector = sector, region=region, model=model, filetype=filetype).iloc[0, 5:].to_numpy(dtype=float) * get_co_to_h2_factor_cmip6(sector)
+    timeseries = None
     for sector in co_ssp_sectors:
-        co_df = get_data_for_component_sector_region_ssp(file, scen, "CO", sector = sector, region=region) 
+        co_df = get_data_for_component_sector_region_ssp(file, scen, "CO", sector = sector, region=region, model=model, filetype=filetype) 
         if co_df.shape[0] == 0:
             continue
-        timeseries = timeseries + co_df.iloc[0, 5:].to_numpy(dtype=float)*get_co_to_h2_factor_cmip6(sector)
+        if timeseries is None:
+            timeseries = co_df.iloc[0, meta_len:].to_numpy(dtype=float)*get_co_to_h2_factor_cmip6(sector)
+        else:
+            timeseries = timeseries + co_df.iloc[0, meta_len:].to_numpy(dtype=float)*get_co_to_h2_factor_cmip6(sector)
     return timeseries
 
 def get_ts_hydrogen_energy_and_mass(file, scen, region="World", model="empty"):
